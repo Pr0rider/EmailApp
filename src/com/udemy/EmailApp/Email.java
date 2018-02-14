0a3cd1e09d2d3b5a0bfd86daf6d2eb306451bcac
Pr0rider <grzegorzgatkowski@gmail.com>
2018-02-14 21:57:35 +0100

    Basic email class. Version 1.0

--- src/com/udemy/EmailApp/Email.java
+++ src/com/udemy/EmailApp/Email.java
@@ -8,8 +8,11 @@ public class Email {
     private String lastName;
     private String password;
     private String department;
-    private int mailboxCapacity;
+    private String email;
+    private int mailboxCapacity = 500;
     private String alternateEmail;
+    private int defaultPasswordLength = 8;
+    private String companySufix = "ivia.com";
 
 
     public Email(String firstName, String lastName) {
@@ -17,6 +20,9 @@ public class Email {
         this.lastName = lastName;
 
         this.department = setDepartment();
+        this.password = randomPassword(defaultPasswordLength);
+
+        email = firstName.toLowerCase() + "." + lastName.toLowerCase() + "@" + department + "." + companySufix;
     }
 
     public String setDepartment() {
@@ -26,21 +32,62 @@ public class Email {
         String department = "";
         switch ( departmentChoice ){
             case 1:
-                department = "Roads";
+                department = "roads";
                 break;
             case 2:
-                department = "Bridges";
+                department = "bridges";
                 break;
             case 3:
-                department = "Managment";
+                department = "managment";
                 break;
             case 0:
-                department = "None";
+                department = "";
                 break;
             default:
-                department = "None";
+                department = "";
                 break;
         }
         return department;
+    }
+
+    private String randomPassword(int length){
+        String passwordSet = "ABCDEFGHIJKLMNOPQRSTUWXYZ0123456789";
+        char [] password = new char [length];
+        for (int i = 0; i < length; i++){
+            int random = (int)(Math.random() * passwordSet.length());
+            password[i] = passwordSet.charAt(random);
+        }
+        return new String (password);
+    }
+
+    public void setMailboxCapacity(int mailboxCapacity) {
+        this.mailboxCapacity = mailboxCapacity;
+    }
+
+    public void setAlternateEmail(String alternateEmail) {
+        this.alternateEmail = alternateEmail;
+    }
+
+    public void changePassword (String password){
+        this.password = password;
+    }
+
+    public int getMailboxCapacity() {
+        return mailboxCapacity;
+    }
+
+    public String getAlternateEmail() {
+        return alternateEmail;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public String showInfo(){
+        return "Display name: " + firstName + " " + lastName +
+                "\nCompany Email: " + email +
+                "\nMailbox capacity " + mailboxCapacity + "mb";
+
     }
 }
--- src/com/udemy/EmailApp/EmailApp.java
+++ src/com/udemy/EmailApp/EmailApp.java
@@ -4,6 +4,7 @@ public class EmailApp {
 
     public static void main(String[] args) {
 
-        Email e1 = new Email ("Janusz", "Wąsacz");
+        Email e1 = new Email ("Janusz", "Kowalski");
+        System.out.println(e1.showInfo());
     }
 }
